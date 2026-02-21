d950fb479a514f8ff1d5c3cc3257230718d99cd4
Sotos2013 <www.sotihatzi@gmail.com>
2026-02-21 23:10:49 +0200

    .....

--- src/dbconnect.php
+++ src/dbconnect.php
@@ -1,18 +1,20 @@
 <?php
-// Συμπερίληψη ρυθμίσεων
+// db_to_json.php
 require_once "db_config.php";
 
-// Σύνδεση με τη νέα απομακρυσμένη βάση δεδομένων
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// Απενεργοποιούμε την εμφάνιση σφαλμάτων στην οθόνη για να μη χαλάνε το JSON
+error_reporting(0);
+ini_set('display_errors', 0);
 
-// Ορισμός charset σε utf8mb4 για τα Ελληνικά (Πολύ σημαντικό για τη νέα βάση!)
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 $mysqli->set_charset("utf8mb4");
 
 if ($mysqli->connect_errno) {
-    die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+    // Αν αποτύχει, στείλε JSON σφάλμα, όχι HTML
+    header('Content-Type: application/json');
+    die(json_encode(["error" => "Connection failed"]));
 }
 
-// Ερώτημα για ανάκτηση δεδομένων (το ίδιο με πριν)
 $sql = "SELECT courses.name AS course_name, 
                course_occurrences.day, 
                course_occurrences.time, 
@@ -21,8 +23,6 @@ $sql = "SELECT courses.name AS course_name,
         INNER JOIN courses ON course_occurrences.course_id = courses.id";
 
 $result = $mysqli->query($sql);
-
-// Δημιουργία πίνακα για την αποθήκευση των δεδομένων
 $data = array();
 
 if ($result) {
@@ -30,34 +30,27 @@ if ($result) {
         $days = explode(", ", $row["day"]);
         $times = explode(", ", $row["time"]);
         $occurrences = array();
-
         foreach ($days as $key => $day) {
-            $occurrences[] = array(
+            $occurrences[] = [
                 "day" => $day,
                 "time" => isset($times[$key]) ? $times[$key] : ""
-            );
+            ];
         }
-
-        $data[] = array(
+        $data[] = [
             "semester" => $row["semester"],
             "name" => $row["course_name"],
             "occurrences" => $occurrences
-        );
+        ];
     }
-    $result->free();
-} else {
-    die("Error executing query: " . $mysqli->error);
 }
 
-// Κλείσιμο σύνδεσης
 $mysqli->close();
 
-// Εγγραφή των δεδομένων στο αρχείο data.json
-$file = 'data.json';
-// Χρησιμοποιούμε JSON_PRETTY_PRINT για να είναι ευανάγνωστο και JSON_UNESCAPED_UNICODE για τα ελληνικά
-if (file_put_contents($file, json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT))) {
-    echo "Το αρχείο data.json δημιουργήθηκε επιτυχώς από τη νέα βάση!";
-} else {
-    echo "Σφάλμα κατά την εγγραφή του αρχείου.";
-}
+// Γράφουμε το αρχείο
+$json_final = json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+file_put_contents('data.json', $json_final);
+
+// ΣΗΜΑΝΤΙΚΟ: Στέλνουμε το JSON και στον browser για το fetch
+header('Content-Type: application/json; charset=utf-8');
+echo $json_final;
 ?>
